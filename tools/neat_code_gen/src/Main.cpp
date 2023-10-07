33a1d96e53a8440187a71d0794beec9f0d33128b
FireFlyForLife <maiko.s.99@gmail.com>
2023-10-08 01:09:21 +0200

    Removed docopt dependency

--- tools/neat_code_gen/src/Main.cpp
+++ tools/neat_code_gen/src/Main.cpp
@@ -10,47 +10,25 @@
 #include <filesystem>
 #include <span>
 
-#include "docopt.h"
 #include "ifc/File.h"
 #include "ifc/Environment.h"
 #include "ifc/MSVCEnvironment.h"
 #include "reflifc/Module.h"
 
 
-constexpr auto USAGE = R"(Usage: 
-    NeatReflectionCodeGen.exe <in_ifc_file> <out_cpp_file>
-    NeatReflectionCodeGen.exe scan <in_dir> <out_dir>
-)";
-
-bool convert_ifc_file(const std::string& ifc_filename, const std::string& cpp_filename) try
+bool convert_ifc_file(const std::filesystem::path& ifc_filename, const std::filesystem::path& cpp_filename) try
 {
-    ContextArea filename_context{ std::format("While loading ifc file: '{0}'. And preparing to output to: '{1}'", ifc_filename, cpp_filename) };
-
-    if (!std::filesystem::exists(ifc_filename))
-    {
-        std::cout << "ERROR: in_ifc_file: '" << ifc_filename << "' does not exist.";
-        return false;
-    }
-    if (!ifc_filename.ends_with(".ifc"))
-    {
-        std::cout << "ERROR: in_ifc_file: '" << ifc_filename << "' is not a .ifc file. It's extension is: " << std::filesystem::path{ ifc_filename }.extension() << '\n';
-        return false;
-    }
+    ContextArea filename_context{ std::format("While loading ifc file: '{0}'. And preparing to output to: '{1}'", ifc_filename.string(), cpp_filename.string()) };
 
     MioBlobHolder ifc_file_blob{ ifc_filename };
     ifc::File ifc_file{ ifc_file_blob.view() };
 
-    ifc::Environment environment{ ifc::read_msvc_config(ifc_filename + ".d.json"), &MioBlobHolder::create_unique };
+    ifc::Environment environment{ ifc::read_msvc_config(ifc_filename.string() + ".d.json"), &MioBlobHolder::create_unique };
    
     std::ofstream file_stream{ cpp_filename, std::ofstream::out | std::ofstream::trunc };
     if (!file_stream.good())
     {
         std::cout << "ERROR: Could not open output file '" << cpp_filename << "' for writing. Reason: " << strerror(errno) << '\n';
-        return false;
-    }
-    if (!cpp_filename.ends_with(".cpp"))
-    {
-        std::cout << "ERROR: out_cpp_file: '" << cpp_filename << "' is not a .cpp file. It's extension is: " << std::filesystem::path{ cpp_filename }.extension() << '\n';
         return false;
     }
 
@@ -70,43 +48,55 @@ catch (...)
     return false;
 }
 
-int main(int argc, char const *argv[])
+struct CodeGenArgs
+{
+    std::filesystem::path input_ifc_path;
+    std::filesystem::path output_cpp_path;
+};
+
+bool parse_command_line_args(std::span<const char*> in_args, CodeGenArgs& out_parsed_args)
+{
+    constexpr auto USAGE = R"(Usage: 
+    NeatReflectionCodeGen.exe <in_ifc_file> <out_cpp_file>)";
+
+    if (in_args.size() < 3) {
+        std::cout << USAGE << '\n';
+        return false;
+    }
+
+    out_parsed_args.input_ifc_path = in_args[1];
+    if (!std::filesystem::exists(out_parsed_args.input_ifc_path)) {
+        std::cout << "ERROR: input ifc file: '" << out_parsed_args.input_ifc_path << "' does not exist.";
+        return false;
+    }
+    if (out_parsed_args.input_ifc_path.extension() != ".ifc") {
+        std::cout << "ERROR: in_ifc_file: '" << out_parsed_args.input_ifc_path 
+            << "' is not a .ifc file. It's extension is: " << out_parsed_args.input_ifc_path.extension() << '\n';
+        return false;
+    }
+
+    out_parsed_args.output_cpp_path = in_args[2];
+    if (out_parsed_args.output_cpp_path.extension() != ".cpp") {
+        std::cout << "ERROR: out_cpp_file: '" << out_parsed_args.output_cpp_path 
+            << "' is not a .cpp file. It's extension is: " << out_parsed_args.output_cpp_path.extension() << '\n';
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, const char* argv[])
 {
     std::cout << "Running NeatReflectionCodeGen!\n";
 
-    const std::vector<std::string> arguments{ argv + 1, argv + argc };
-    docopt::Options parsed = docopt::docopt(USAGE, arguments, true, "0.2");
-
-    if (parsed["scan"].asBool())
-    {
-        const std::filesystem::path target_dir = parsed["<in_dir>"].asString();
-        const std::filesystem::path output_dir = parsed["<out_dir>"].asString();
-
-        for (auto entry : std::filesystem::directory_iterator{ target_dir })
-        {
-            if (entry.is_regular_file() && entry.path().extension() == ".ifc")
-            {
-                auto output_filename = std::filesystem::absolute(output_dir / entry.path().filename().replace_extension("cpp"));
-                
-                std::cout << "Converting '" << std::filesystem::absolute(entry) << "' to '" << output_filename << "'\n";
-
-                if (!convert_ifc_file( entry.path().string(), output_filename.string()))
-                {
-                    std::cout << "ERROR: Failed to convert '" << entry << "'\n";
-                    return 1;
-                }
-            }
-        }
+    CodeGenArgs code_gen_args{};
+    if(!parse_command_line_args({ argv, (size_t)argc }, code_gen_args)) {
+        return 1;
     }
-    else 
-    {
-        const std::string ifc = parsed["<in_ifc_file>"].asString();
-        const std::string cpp = parsed["<out_cpp_file>"].asString();
 
-        if (!convert_ifc_file(ifc, cpp))
-        {
-            return 1;
-        }
+    if (!convert_ifc_file(code_gen_args.input_ifc_path, code_gen_args.output_cpp_path))
+    {
+        return 1;
     }
 
     return 0;
